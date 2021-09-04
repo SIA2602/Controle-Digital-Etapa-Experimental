f83faf6796bf908f9a5406478895b6cdefddb2f0
Sia2602 <danilojose26@hotmail.com>
2021-09-04 18:40:34 -0300

    Adicionando Arquivos Base.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Controle-Digital-Etapa-Experimental
 Trabalho Pratico para a disciplina de Controle Digital
+
+# Conteudo da Pasta
+
+- Trabalho Etapa II (EXPERIMENTAL) [Pasta Contendo a implementacao da segunda estapa do tranalho pratico]
+    - Ensaios_Sistema [Pasta Contendo as medicoes de velocidade por tensao. Usados para gerar uma reta de tensao x rpm com o intuito de achar o ganho estatico do sistema]
+    - Leitura_Velocidade [Pasta que contem um programa em arduino para acionar o motor CC]
+    - Relatorio_Jupiter [Pasta com Relatorio final contendo toda descricao do que foi feito nessa ultima etapa.]
+    - Enunciado [Enunciado do do Trabalho pratico]
---
+++ Trabalho Etapa II (EXPERIMENTAL)/Leitura_Velocidade/Leitura_Velocidade.ino
@@ -0,0 +1,60 @@
+//variaveis para motor
+int pino = 5;
+
+int valuePWM = 0;
+double cicloTrabalho = (valuePWM*100)/255;
+double Vout = 5*(cicloTrabalho/100);
+int speedAtual = valuePWM;
+
+//variaveis para sensor encoder
+int pino_encoder = 2;
+long int rpm;
+volatile byte pulsos;
+unsigned long timeold;
+unsigned int pulsos_por_volta = 2; //numeros de fresta do disco encoder
+int intervalo = 500;
+
+void contador()
+{
+  pulsos++;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  //configurando pinos
+  pinMode(pino, OUTPUT);
+  pinMode(pino_encoder, INPUT);
+
+  //Interrupcao para 0 = Pino digital 2
+  attachInterrupt(0, contador, RISING);
+
+  pulsos = 0;
+  rpm = 0;
+  timeold = 0;
+  
+  while(!Serial);
+  analogWrite(pino, speedAtual);
+  //Serial.println("Velocidade > 0 e < 255");    
+}
+
+void loop() { 
+  //speedAtual = Serial.parseInt();
+  /*if(speedAtual >= 0 && speedAtual <= 255){             
+    analogWrite(pino, speedAtual);             
+  }*/
+   
+  if(millis() - timeold >= intervalo) {
+    //desabilitando interrupcao durante o calculo
+    detachInterrupt(0);
+    rpm = (60*intervalo / pulsos_por_volta)/(millis()-timeold)*pulsos;
+    timeold = millis();
+    pulsos = 0;    
+    attachInterrupt(0, contador, RISING);
+  }  
+  
+  Serial.print(Vout); 
+  Serial.print(" ");  
+  Serial.println(rpm);   
+  delay(100); 
+}
